e04fe05e6f75cab5015c601baae45d2165ae5772
u1601 <tagawa.takayoshi@ujrc.co.jp>
2024-10-07 14:05:15 +0900

    Fix: (myutils.py) PylanceのWarning抑制、不具合修正

--- myutils.py
+++ myutils.py
@@ -19,7 +19,7 @@ ToDo:
     *
 
 """
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 __author__ = 't-nakayoshi (Takayoshi Tagawa)'
 
 import argparse
@@ -28,8 +28,8 @@ import platform
 import re
 import sys
 
-_PS1_FILE = r'.\myFolders.ps1'
-_PS1_SCRIPT = '''#
+_PS1_FILE:str = r'.\myFolders.ps1'
+_PS1_SCRIPT:str = '''#
 $shellapp = New-Object -ComObject Shell.Application
 $shellapp.Namespace("shell:Personal").Self.Path
 $shellapp.Namespace("shell:My Music").Self.Path
@@ -43,23 +43,20 @@ def platform_info() -> tuple:
     """実行環境のOS名、バージョン（リリース）、Pythonのバージョン情報を取得する
     Args:
         none
-
     Returns:
         OS情報 (tuple): ('<OS名>', '<バージョン(リリース)>', '<Pythonのバージョン>')
-    
     Examples:
         >>> Windows = ('Windows', '10', '3.10.11')              - Windows 10
         >>> Mac OS  = ('Darwin', '18.2.0', '3.10.11')           - Mojave 10.14.2
         >>> Linux   = ('Linux', '4.15.0-42-generic', '3.10.11') - Ubuntu 18.04.1 LTS
     """
-    return platform.system(), platform.release(), platform.python_version()
+    return (platform.system(), platform.release(), platform.python_version())
 
 
 def get_special_directory() -> tuple:
     """特殊ディレクトリのパスを取得する
     Args:
         none
-
     Returns:
         特殊ディレクトリパス (tuple): ('<My Documents>', '<My Music>', '<My Pictures>', '<My Videos>', '<Downloads>')
     """
@@ -77,7 +74,7 @@ def get_special_directory() -> tuple:
         shell: str = r'pwsh' if os.path.exists(PWSH7) else r'powershell'
         folders: list = os.popen(rf'{shell} {_PS1_FILE}').read().rstrip('\n').split('\n')
     else:
-        import glib
+        import glib # type: ignore
         folders: list = [
             glib.get_user_special_dir(glib.USER_DIRECTORY_DOCUMENTS),
             glib.get_user_special_dir(glib.USER_DIRECTORY_MUSIC).
@@ -93,13 +90,10 @@ def get_running_path() -> tuple:
     """実行時ディレクトリとコンソールの有無を取得する
     * EXEを展開した一時ディレクトリのPathを返す
     * スクリプト実行の場合はスクリプトのPathを返す
-
     Arguments:
         none
-
     Returns:
         実行時ディレクトリとコンソール有無のtuple
-
     Note:
         * コンソールの有無は暫定
         * EXEファイルがあるディレクトリは、os.path.dirname(sys.argv[0]) で取得する
@@ -108,16 +102,16 @@ def get_running_path() -> tuple:
     try:
         """EXE化した場合のTenporary実行時ディレクトリを取得
         """
-        base_path = sys._MEIPASS
+        base_path:str = sys._MEIPASS    # type: ignore
     except:
         # スクリプト実行パスを取得
-        base_path = os.path.dirname(__file__)
+        base_path:str = os.path.dirname(__file__)
 
-    no_console = False
+    no_console:bool = False
     if sys.stderr.fileno() != 2:
         no_console = True
 
-    return base_path, no_console
+    return (base_path, no_console)
 
 
 class IsFileAction(argparse.Action):
@@ -164,12 +158,12 @@ class IsParentAction(argparse.Action):
         """
         if nargs is not None and nargs != '?':
             raise ValueError('Invalid `nargs`: multiple arguments not allowed.')
-        super(IsDirAction, self).__init__(option_strings, dest, **kwargs)
+        super(IsParentAction, self).__init__(option_strings, dest, **kwargs)
 
     def __call__(self, parser, namespace, values, option_string=None):
         """親ディレクトリが存在しなければエラー
         """
-        parent = os.path.dirname(str(values))
+        parent:str = os.path.dirname(str(values))
         if len(parent) == 0:
             parent = '..'
         if not os.path.isdir(parent):
@@ -177,29 +171,25 @@ class IsParentAction(argparse.Action):
         setattr(namespace, self.dest, values)
 
 
-def scan_directory(directory: str, ptn: str='', recursive: bool=True) -> list[str]:
+def scan_directory(directory:str, ptn:str='', recursive:bool=True) -> list[str]:
     """ファイルの抽出
     指定ディレクトリ内のファイルをリストアップする。（昇順）
-
     Args:
         directory: ディレクトリ名
         ptn (str) : ファイル名フィルタ（正規表現）
         recursive (bool): 再帰検索フラグ（True=再帰）
-
     Returns:
         list: ファイル名のリスト
-
     Exsamples:
         >>> files = scan_directory('abc')
-
     """
-    files = []
+    files:list[str] = []
     reg = None
     if len(ptn) > 0:
         reg = re.compile(ptn)
 
     for f in os.listdir(directory):
-        child = os.path.join(directory, f)
+        child:str = os.path.join(directory, f)
         if os.path.isdir(child) and recursive:
             files += scan_directory(child, ptn)
         elif os.path.isfile(child):
@@ -209,7 +199,7 @@ def scan_directory(directory: str, ptn: str='', recursive: bool=True) -> list[st
                 if reg.search(child, re.IGNORECASE):
                     files.append(child)
 
-    if len(files) != 0:
+    if files:
         files.sort(key=natural_keys)
 
     return files
@@ -217,14 +207,12 @@ def scan_directory(directory: str, ptn: str='', recursive: bool=True) -> list[st
 
 def is_abort() -> bool:
     """中断確認
-
     Args:
         None
-
     Returns:
         bool: 結果（True=中断）
     """
-    ans = input('Abort? Y or [N]: ')
+    ans:str = input('Abort? Y or [N]: ')
     if len(ans) == 0 or ans[0].lower() == 'y':
         return True
     else:
